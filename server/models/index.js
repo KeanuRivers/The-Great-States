66bf931e084b587dbbfd8e9efc5780ca6e5633ea
Keanu Rivers <keanurivers404@gmail.com>
2022-11-29 19:08:04 -0700

    updated user code and made path to state.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require ("mongoose")
 const User = require("../models/User")
+const State = require("../models/States")
+
 
 mongoose.connect("mongodb://localhost/states");
 
-new User({ name:"Keanu", email:"keanu22@gmail.com}"})
