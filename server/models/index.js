319118b1079c95e1535e1808517de212774b5274
Keanu Rivers <keanurivers404@gmail.com>
2022-12-02 11:35:09 -0700

    updated path to state and user

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require ("mongoose")
 const User = require("../models/User")
-const State = require("../models/States")
+const States = require("../models/States")
 
 
-mongoose.connect("mongodb://localhost/states");
+
 
