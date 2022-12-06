552f239b6f573faad5458e49ae1ababf66f4c7e8
Davg1700 <davg1700@gmail.com>
2022-12-05 19:35:55 -0700

    remomoved components

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,27 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-import register from '../components/register';
+function Register() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicUser">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="username" placeholder="Enter username" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default Register;
